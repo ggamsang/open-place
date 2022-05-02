a3e8c21a9772f7ca3d05778b94f9f2a5c5d61b6c
catchico <iana6528@gmail.com>
2022-05-02 15:01:01 +0900

    폴더 추가

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ClientTemplate from 'clientTemplate/ClientTemplate'
+import MainContainer from 'containers/MainContainer/MainContainer'
+
+class MainPage extends Component {
+  render() {
+    return (
+    <ClientTemplate>
+        <MainContainer {...this.props} />
+    </ClientTemplate>)
+  }
+}
+
+export default MainPage;
