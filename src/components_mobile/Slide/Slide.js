9b8a8112da29ae7ed2829f253f153e41ae97c2a5
ggamsang <w.ggmsng@gmail.com>
2022-05-10 14:14:42 +0900

    carousel modifying

--- src/components_mobile/Slide/Slide.js
+++ src/components_mobile/Slide/Slide.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import img0 from "resources/sample-image-01.png";
+import arrow from "resources/arrow-right.png";
 
+const WIDTH = 335
 const Wrapper = styled.div`
     margin: auto;
     margin-top: 10px;
-    width: 360px;
+    width: ${WIDTH}px;
     height: 187px;
     border-radius: 6px;
     overflow-y: hidden;
@@ -41,6 +43,31 @@ const Wrapper = styled.div`
 	display: flex;
 	-webkit-overflow-scrolling: touch;
 	overflow-x: scroll;
+
+    .pagination {
+        position: fixed;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        width: 56px;
+        height: 8px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        transform: translate(255%,2000%);
+        li {
+            opacity: 0.9;
+            background-color: #AAA;
+            width: 8px;
+            height: 8px;
+            border-radius: 100%;
+        }
+        .active {
+            opacity: 1;
+            background-color: #ECF1F7;
+        }
+        // border: 1px solid green;
+    }
 `;
 const Slide = styled.div`
     min-width: 360px;
@@ -54,9 +81,11 @@ const Slide = styled.div`
     background-size: cover;
 	position: relative;
     .para1 {
+        // width: 156px;
+        // height: 44px;
         position: absolute;
-        left: 17px; 
-        top: 65px;       
+        left: 12px;
+        top: 113px;       
         font-family: Montserrat;
         font-weight: 700;
         font-size: 20px;
@@ -64,20 +93,27 @@ const Slide = styled.div`
         text-align: left;
     }
     .para2 {
-        width: 99.39px;
-        height: 34.79px;
         position: absolute;
-        top: 69.61px;
-        right: 15.31px;
-        border-radius: 33px;
-        background-image: linear-gradient(132deg, #9672FB, #D772FB);
+        top: 116px;
+        right: 30px;
         font-family: Pretendard;
-        font-weight: 400;
+        font-weight: 500;
         font-size: 17px;
         line-height: 16px;
         display: flex;
+        flex-direction: column;
         justify-content: center;
         align-items: center;
+        .arrow {
+            width: 100%;
+            display: flex;
+            img {
+                margin-top: 2px;
+                margin-left: auto;
+                width: 18px;
+                height: 11px;
+            }
+        }
     }
 `;
 
@@ -85,22 +121,37 @@ const strDETAILS = "더 알아보기";
 const strNewExperience1 = "매일 새로운 경험";
 const strNewExperience2 = "OPEN MARKET";
 class Slider extends React.Component {
+    getwidth = () => {
+        const node = document.getElementById('slide');
+        console.log(node.scrollLeft);
+    }
     render() {
-        return (<Wrapper>
-            <ul className='dots'>
-                <li>.</li>
-                <li>.</li>
-                <li>.</li>
-            </ul>
-            <Slide url={img0} >
-                <div className='para1'>{strNewExperience1}<br />{strNewExperience2}</div>
-                <div className='para2'>{strDETAILS}</div>
-            </Slide>
-            <Slide url={img0} />
-            <Slide url={img0} />
-            <Slide url={img0} />
+        return (
+            <Wrapper id="slide" onClick={() => this.getwidth()}>
 
-        </Wrapper>);
+                <Slide url={img0}>
+                    <div className='para1'>
+                        {strNewExperience1}<br />
+                        {strNewExperience2}
+                    </div>
+                    <div className='para2'>
+                        {strDETAILS}
+                        <div className="arrow">
+                            <img alt="arrow-button" src={arrow} />
+                        </div>
+                    </div>
+                </Slide>
+                <Slide url={img0} />
+                <Slide url={img0} />
+                <Slide url={img0} />
+
+                <ul className='pagination'>
+                    <li className='active'>&nbsp;</li>
+                    <li>&nbsp;</li>
+                    <li>&nbsp;</li>
+                    <li>&nbsp;</li>
+                </ul>
+            </Wrapper>);
     }
 }
 export default Slider;
