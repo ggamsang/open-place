03ca264808ca60d73c09335fec8d8d96929c1955
ggamsang <w.ggmsng@gmail.com>
2022-05-10 12:16:03 +0900

    search-bar modified :)

--- src/components_mobile/Search/Search.js
+++ src/components_mobile/Search/Search.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styledComponents from 'styled-components';
+import zoombg from "resources/zoom.png";
+import filterbg from "resources/filter.png";
 
 const Wrapper = styledComponents.div`
     width: 100vw;
@@ -7,42 +9,78 @@ const Wrapper = styledComponents.div`
 const InputWrapper = styledComponents.div`
     margin: auto;
     display: flex;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center;
     max-width: 360px;
     width: 360px;
     height: 35px;
     border-radius: 6px;
     background-color: white;
+
     
-    // padding-left: 9.53px;
-    // padding-right: 16.55px;
     input {
+        font-family: Pretendard;
+        font-weight: 500;
+        font-size: 15px;
+        text-align: left;
+        line-height: 18px;
+        border: none;
+        outline: none;
+        :focus {
+            outline: none !important;
+            border: 1px solid #BDBDBD;
+            // box-shadow: 0 0 1px #719ECE;
+        }
         height: 18px;
         padding: 0px;
-        // width: 100%;
+        width: 80%;
+        ::placeholder {
+            color: #BDBDBD;
+
+        }
     }
     .zoom {
-        border:1px dashed #BDBDBD; 
-        width: 18.69px; 
-        height: 18.69px; 
+        // border:1px dashed #BDBDBD;
+        background-image: url(${zoombg});
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 19px; 
+        height: 19px; 
+        margin-left: 10px;
+        margin-right: 15px;
     }
     .filter { 
-        border:1px dashed #BDBDBD; 
-        width: 23.14px; 
-        height: 18.62px; 
+        // border: 1px dashed #BDBDBD; 
+        background-image: url(${filterbg});
+        background-size: cover;
+        background-repeat: no-repeat;
+        width: 23px; 
+        height: 18px;
+        margin-right: 15px;
+        margin-left: 15px;
     }
 `;
 const Zoom = () => <div style={{}}>&nbsp;</div>
 const Filter = () => <div style={{}}>&nbsp;</div>
 
 class Search extends React.Component {
+    handleKeyDown = e => {
+        if (e.key === "Enter") {
+            alert(e.target.value);
+        }
+    }
+    handleZoomClicked = () => {
+        const input = document.getElementById("search");
+        input.value && alert(input.value);
+    }
     render() {
         return (<Wrapper>
             <InputWrapper>
-                <span className="zoom">
+                <span className="zoom" onClick={() => this.handleZoomClicked()}>
                     <Zoom />
                 </span>
-                <input placeholder='경험 찾아보기' />
+                <input id="search" placeholder='경험 찾아보기' onKeyDown={e => this.handleKeyDown(e)} />
                 <span className='filter'>
                     <Filter />
                 </span>
