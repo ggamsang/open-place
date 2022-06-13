be0ebbcfb8283d13efff1b1757f5b446210a1dbe
ggamsang <w.ggmsng@gmail.com>
2022-06-13 11:03:07 +0900

    community - pagination added

--- src/components_mobile/ArticleList/ArticleList.js
+++ src/components_mobile/ArticleList/ArticleList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Pagination } from 'semantic-ui-react';
+// import Pagination from 'semantic-ui-react-button-pagination';
 import { goto } from 'smallfuncs';
 import styled from 'styled-components';
 
@@ -60,6 +62,23 @@ const Wrapper = styled.div`
             color: #000;
         }
     }
+
+    .page-wrap {
+        margin-top: 20px;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        .active {
+            background-color: red;
+            color: white;
+        }
+        .item {
+            padding: 5px 12px;
+            margin-right: 10px;
+            border-radius: 5px;
+        }
+    }
 `;
 const Thumbnail = styled.div`
     border: none;
@@ -77,27 +96,18 @@ const dummy = [
     { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
     { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
     { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
-    { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
-    { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
-    { "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19", },
-    {
-        "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19",
-    },
-    {
-        "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19",
-    },
-    {
-        "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19",
-    },
-    {
-        "header": "말머리", "title": "제목예시", "url": "https://i.picsum.photos/id/1011/5472/3648.jpg?hmac=Koo9845x2akkVzVFX3xxAc9BCkeGYA9VRVfLE4f0Zzk", "author": "국민대CRC", "date": "2022-05-19",
-    },
 ];
 
 class ArticleList extends React.Component {
+    constructor() {
+        super();
+        this.state = { offset: 0 };
+    }
     gotoDetail = id => {
         goto("READ", id);
     }
+    handleClick = offset => console.log(offset);
+
     render() {
 
         const { articles = dummy } = this.props;
@@ -122,6 +132,19 @@ class ArticleList extends React.Component {
                     </div>
                 </div>)}
 
+            <div className='page-wrap'>
+                <Pagination
+                    boundaryRange={0}
+                    defaultActivePage={1}
+                    ellipsisItem={null}
+                    firstItem={null}
+                    lastItem={null}
+                    siblingRange={1}
+                    totalPages={10}
+                    onPageChange={this.handleClick}
+
+                />
+            </div>
         </Wrapper>)
     }
 }
