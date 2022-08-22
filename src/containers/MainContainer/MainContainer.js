f8fb79aa99bfb14d58335460cba6587a5298f08a
ggamsang <ggmsng@ggmsngs-MacBook-Pro.local>
2022-08-22 11:36:01 +0900

    main page - split into desktop and mobile

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Main from 'components/Main/Main'
+import MainMobile from "components_mobile/Main/Main";
 
 class MainContainer extends Component {
   render() {
     return (<React.Fragment>
-      <Main />
+      {window.innerWidth > 500
+        ? <Main />
+        : <MainMobile />}
     </React.Fragment>)
   }
 }
