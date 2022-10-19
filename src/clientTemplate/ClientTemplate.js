cfbe110b991e2c1b6e95fda72f2224988778f17c
ggamsang <ggmsng@ggmsngs-MacBook-Pro.local>
2022-10-19 13:23:18 +0900

    mobile version revival

--- src/clientTemplate/ClientTemplate.js
+++ src/clientTemplate/ClientTemplate.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import styled from "styled-components";
-import Footer from "components/Footer"
+import Footer from "components/Footer";
 import TopMenu from "components/Menu/Top";
+import BottomMenu from "components_mobile/Menu/Bottom";
 
 const Wrapper = styled.div`
   z-index: 1;
@@ -15,7 +16,7 @@ const Wrapper = styled.div`
   }
   .main {
     z-index: 5;
-    margin-top: 103px; 
+    margin-top: 103px;
     min-height: 100vh;
     margin-bottom: 25px;
   }
@@ -23,7 +24,6 @@ const Wrapper = styled.div`
     pointer-events: none;
   }
   .bottom {
-
     .blanker {
       height: 50px;
     }
@@ -47,80 +47,94 @@ const Wrapper = styled.div`
 `;
 
 class ClientTemplate extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     stick: false,
-  //     prev_y: 0,
-  //   };
-  // }
-  // componentDidMount() {
-  //   // window.addEventListener('scroll', e => this.handleScroll(e), true);
-  //   window.addEventListener('scroll', this.scroll, true);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stick: false,
+      prev_y: 0,
+    };
+  }
+  componentDidMount() {
+    // window.addEventListener('scroll', e => this.handleScroll(e), true);
+    window.addEventListener("scroll", this.scroll, true);
+  }
+  handleScroll = async (_) => {
+    // get main height
+    const main = document.getElementById("main");
+    const rectMain = main.getBoundingClientRect();
+    if (!rectMain) {
+      return;
+    }
+    // get bottom menu position
+    const nav = document.getElementById("nav");
+    const rectNav = nav.getBoundingClientRect();
+    if (!rectNav) {
+      return;
+    }
+    // detect direction and store prev position
+    // const { prev_y } = this.state;
+    // let dir = "";
+    // if (rectMain.y - prev_y > 0) {
+    //   dir = "UP";
+    // } else {
+    //   dir = "DOWN";
+    // }
+    this.setState({ prev_y: rectMain.y });
+    console.log(new Date().getTime(), 0);
 
-  // handleScroll = async (_) => {
-  //   // get main height
-  //   const main = document.getElementById('main');
-  //   const rectMain = main.getBoundingClientRect();
-  //   if (!rectMain) {
-  //     return;
-  //   }
-  //   // get bottom menu position
-  //   const nav = document.getElementById('nav');
-  //   const rectNav = nav.getBoundingClientRect();
-  //   if (!rectNav) {
-  //     return;
-  //   }
-  //   // detect direction and store prev position
-  //   // const { prev_y } = this.state;
-  //   // let dir = "";
-  //   // if (rectMain.y - prev_y > 0) {
-  //   //   dir = "UP";
-  //   // } else {
-  //   //   dir = "DOWN";
-  //   // }
-  //   this.setState({ prev_y: rectMain.y });
-  //   console.log(new Date().getTime(), 0);
-
-  //   const { stick, } = this.state;
-  //   if (stick === false) {
-  //     if (rectMain.height - (rectNav.y - rectMain.y) < 50) {
-  //       console.log(new Date().getTime(), 1);
-  //       this.setState({ stick: true });
-  //     }
-  //   } else {
-  //     if (rectMain.height - (rectNav.y - rectMain.y) < 50) {
-  //       console.log(new Date().getTime(), 3);
-  //       setTimeout(this.setState({ stick: false }), 500);
-  //     }
-  //   }
-  // }
+    const { stick } = this.state;
+    if (stick === false) {
+      if (rectMain.height - (rectNav.y - rectMain.y) < 50) {
+        console.log(new Date().getTime(), 1);
+        this.setState({ stick: true });
+      }
+    } else {
+      if (rectMain.height - (rectNav.y - rectMain.y) < 50) {
+        console.log(new Date().getTime(), 3);
+        setTimeout(this.setState({ stick: false }), 500);
+      }
+    }
+  };
 
   render() {
+    return window.innerWidth > 500 ? (
+      <Wrapper id="client-template">
+        <div className="top">
+          <TopMenu
+            login={this.props.isLoggedIn}
+            userInfo={this.props.userInfo}
+          />
+        </div>
 
-    return (<Wrapper id="client-template">
-      <div className='top'>
-        <TopMenu login={this.props.isLoggedIn} userInfo={this.props.userInfo} />
-      </div>
+        <div id="main" className="main">
+          {this.props.children}
+          <Footer />
+        </div>
 
-      <div id="main" className='main'>
+        {/* <div className='bottom'> */}
+        {/* </div> */}
+      </Wrapper>
+    ) : (
+      <Wrapper id="client-template">
+        <div id="main" className="main">
+          {this.props.children}
+        </div>
 
-        {this.props.children}
-        <Footer />
-      </div>
+        <div id="nav" className={`nav ${this.state.stick ? "stick" : ""}`}>
+          <BottomMenu up={this.state.stick} login={this.props.isLoggedIn} />
+        </div>
 
-      {/* <div className='bottom'> */}
-      {/* </div> */}
-
-    </Wrapper>);
+        <div className="bottom">
+          <Footer />
+        </div>
+      </Wrapper>
+    );
   }
 }
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   isLoggedIn: state.Authentication.status.isLoggedIn,
   userInfo: state.Authentication.status.userInfo,
 });
-const mapDispatchToProps = dispatch => ({
-});
+const mapDispatchToProps = (dispatch) => ({});
 export default connect(mapStateToProps, mapDispatchToProps)(ClientTemplate);
