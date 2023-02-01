81e6212b07efec33ea8cdc9dc013a501bbf5a664
ggamsang <w.ggmsng@gmail.com>
2023-02-01 15:14:16 +0900

    socket

--- src/mobile/modules/Socket/index.jsx
+++ src/mobile/modules/Socket/index.jsx
@@ -3,6 +3,7 @@ import host from "../../../config";
 
 export default class Socket {
   constructor(ns) {
+    alert(host, ns);
     this.socket = io(host.replace("/api", ns ? `/${ns}` : ``), {
       path: "/socket.io",
       transports: ["websocket", "polling", "flashsocket"],
